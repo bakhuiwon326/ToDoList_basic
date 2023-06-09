f539b810e7ccc9a53b255c597f34087ee25b7f06
bakhuiwon326 <powerup326@naver.com>
2023-06-09 17:29:02 +0900

    프론트엔드 기본기능 api 적용

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -4,78 +4,33 @@ import Footer from '../src/component/Footer';
 import Todo from '../src/component/Todo';
 import { useEffect, useState } from 'react';
 import AddTodo from './component/AddTodo';
+import { call } from "./service/ApiService";
 
 var idSequence = 0;
 
 function App(){
-  const exItems = [
-    {
-      id: 0,
-      done: false,
-      title: "0번째 todo 입니다."
-    },
-    {
-      id: 1,
-      done: false,
-      title: "1번째 todo 입니다."
-    },
-    {
-      id: 2,
-      done: true,
-      title: "2번째 todo 입니다."
-    },
-    {
-      id: 3, 
-      done: false,
-      title: "3번째 todo 입니다."
-    }
-  ]
 
-  const [itemList, setItemList] = useState([]);
+  const [itemList, setItemList] = useState({items:[]});
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      headers: {"Content-Type": "application/json"}
-    }
-    
-    fetch("http://localhost:8080/todo", requestOptions)
-    .then((response) => response.json())
-    .then((response) => {
-      setItemList(response.data);
-    }, (error) => {
-      console.log(`에러발생: ${error}`);
-    })
+    call("/todo", "GET", null).then((res) => setItemList({items: res.data}));  
   }, []);
   
   useEffect(() => {
-    
     console.log(`화면에 출력되는 itemList가 변경되었습니다.`);
     console.log(itemList);
   }, [itemList]);
 
   const addTodoFunction = (newItem) => {
-    newItem.id = "ID-" + idSequence;
-    idSequence++;
-    newItem.done = false;
-    setItemList([...itemList, newItem])
+    call("/todo", "POST", newItem).then((res) => setItemList({items: res.data}));  
   };
 
   const deleteTodoFunction = (deleteItem) =>{
-    var newItemList = [];
-    itemList.map((itm) => {
-      if(itm.id !== deleteItem.id) newItemList.push(itm);
-    })
-    setItemList(newItemList);
+    call("/todo", "DELETE", deleteItem).then((res) => setItemList({items: res.data}));  
   }
 
   const modifyTodoFunction = (modifyItem) =>{
-    var newItemList = [];
-    itemList.map((itm) => {
-      if(itm.id === modifyItem.id) newItemList.push(modifyItem)
-      else newItemList.push(itm);
-    })
-    setItemList(newItemList);
+    call("/todo", "PUT", modifyItem).then((res) => setItemList({items: res.data}));  
   }
 
   return (
@@ -83,7 +38,7 @@ function App(){
       <Header/>
         <AddTodo addItem={addTodoFunction}/>
         {
-          itemList.map((itm) => (
+          itemList.items.map((itm) => (
             <Todo item={itm} deleteItem={deleteTodoFunction} modifyItem={modifyTodoFunction}/>
           ))
         }
